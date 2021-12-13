4f898a422566ecd80409317f4408034d56a5098a
AugustinBordy <augustin.bordy@kalyzee.com>
2021-12-13 11:07:36 +0100

    feat: Registering new mixer actions

--- src/kast_modules/audioManagement.js
+++ src/kast_modules/audioManagement.js
@@ -8,6 +8,18 @@ module.exports = function(socket) {
 
     this.actions = [
         Generator.createAction('context'),
+
+        Generator.createAction('set_input_gain'),
+        Generator.createAction('set_input_toggled'),
+
+        Generator.createAction('set_child_level'),
+
+        Generator.createAction('set_output_volume'),
+        Generator.createAction('set_output_toggled'),
+
+        Generator.createAction('set_input_to_output'),
+
+        // Legacy :
         Generator.createAction('volume_up'),
         Generator.createAction('volume_down'),
         Generator.createAction('boost_up'),
